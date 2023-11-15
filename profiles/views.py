ff715c1fe2522b4af99945b418e2ca5a36b876e7
Roger Davis <roger.davis86@yahoo.com>
2023-11-15 23:21:20 +0000

    create comment generic views. Update posts and profiles to use generic views.

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,29 @@
 from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from duggiezb.permissions import IsOwnerOrReadOnly
 
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
-class ProfileList(APIView):
-
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context = {'request': request})
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
+
+    def get_object(self):
         try:
-            profile = Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=self.kwargs['pk'])
             self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context = {'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data = request.data, context = {'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        profile = self.get_object()
+        self.check_object_permissions(self.request, profile)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
