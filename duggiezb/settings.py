46cb626e04c54b277bf52359ad183aa51db16a0c
Roger Davis <roger.davis86@yahoo.com>
2023-11-17 11:47:47 +0000

    troubleshooting.

--- duggiezb/settings.py
+++ duggiezb/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
